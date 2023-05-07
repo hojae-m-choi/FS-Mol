719f8c0f6e891bc50b9017f87a523f43edeeca5b
hojae-m-choi <chjko0206@gmail.com>
2023-05-07 23:59:46 +0000

    fix) test script for multi-task and maml

---
+++ scripts/multitask_test.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+export CUDA_VISIBLE_DEVICES=1
+python fs_mol/multitask_test.py outputs/FSMol_Multitask_2023-05-03_07-49-15/best_model.pt\
+        datasets/fs-mol/\
+        --regression-task --metric-to-use rmse
+# python fs_mol/multitask_test.py TRAINED_MODEL DATA_PATH
