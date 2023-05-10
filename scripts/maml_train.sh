7aae854aa2ec3e79c739d38e86ca938a62ea9a5d
hojae-m-choi <chjko0206@gmail.com>
2023-05-10 00:58:50 +0000

    mod) refactoring train/test scripts

--- scripts/maml_train.sh
+++ scripts/maml_train.sh
@@ -2,4 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=1
 export LD_LIBARY_PATH=/anaconda/envs/fsmol/lib:$LD_LIBARY_PATH
-python fs_mol/maml_train.py datasets/fs-mol/ --regression-task --test-metric rmse
+python fs_mol/maml_train.py\
+    datasets/fs-mol/\
+    --regression-task\
+    --test-metric rmse
