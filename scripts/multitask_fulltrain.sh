2426d35213ec038f6c1df99d495dc598006198ed
hojae-m-choi <chjko0206@gmail.com>
2023-05-16 08:24:21 +0000

    fix) rescaling

--- scripts/multitask_fulltrain.sh
+++ scripts/multitask_fulltrain.sh
@@ -5,4 +5,5 @@ export CUDA_VISIBLE_DEVICES=1
 python fs_mol/multitask_train.py\
     datasets/fs-mol/\
     --regression-task\
+    --task-list-file datasets/entire_train_set.json\
     --metric-to-use rmse
